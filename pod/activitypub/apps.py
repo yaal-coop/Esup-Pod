3423b692b4e69c00e02c6b120cd9abad2349b2b2
Éloi Rivard <eloi@yaal.coop>
2024-02-16 11:09:44 +0100

    keypair generation

--- pod/activitypub/apps.py
+++ pod/activitypub/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ActivitypubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pod.activitypub'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pod.activitypub"
