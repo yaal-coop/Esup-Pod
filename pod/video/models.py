f7a632a91eae961528b3b5aa59c5937213d3d576
Éloi Rivard <eloi@yaal.coop>
2024-03-22 11:00:52 +0100

    c'est un peu mieux

--- pod/video/models.py
+++ pod/video/models.py
@@ -1001,7 +1001,7 @@ class Video(models.Model):
         """
         return 360 if self.is_video else 244
 
-    def get_thumbnail_url(self) -> str:
+    def get_thumbnail_url(self, scheme=False) -> str:
         """Get a thumbnail url for the video."""
         request = None
         if self.thumbnail and self.thumbnail.file_exist():
@@ -1020,6 +1020,11 @@ class Video(models.Model):
                     static(DEFAULT_THUMBNAIL),
                 ]
             )
+
+        if scheme:
+            scheme = "https" if getattr(settings, "SECURE_SSL_REDIRECT") else "http"
+            return f"{scheme}:{thumbnail_url}"
+
         return thumbnail_url
 
     @property
