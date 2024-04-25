22c2727473f038cb3c3010ae19974889678a832e
Loan Robert <loan.rbrt@gmail.com>
2024-04-25 11:08:39 +0200

    Add external video model to reference following instance source video

--- pod/activitypub/models.py
+++ pod/activitypub/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from pod.video.models import Video
 
 class Follower(models.Model):
     actor = models.CharField(
@@ -15,4 +16,18 @@ class Following(models.Model):
         _("Object"),
         max_length=255,
         help_text=_("Followed object"),
+    )
+
+
+class ExternalVideo(Video):
+    source_instance = models.ForeignKey(
+        Following,
+        on_delete=models.CASCADE,
+        verbose_name=_("Source instance"),
+        help_text=_("Video origin instance"),
+    )
+    stream = models.CharField(
+        _("Stream url"),
+        max_length=255,
+        help_text=_("External instance video stream"),
     )
