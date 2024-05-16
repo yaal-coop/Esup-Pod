92a144c119afcd9cee0232fe271da07dbe1d241c
Éloi Rivard <eloi@yaal.coop>
2024-05-16 15:44:25 +0200

    on dirait que je fous le bordel mais en fait je range

--- pod/activitypub/tasks.py
+++ pod/activitypub/tasks.py
@@ -8,6 +8,7 @@ except ImportError:
     from .. import settings as settings_local
 
 from celery import Celery
+from celery.utils.log import get_task_logger
 
 ACTIVITYPUB_CELERY_BROKER_URL = getattr(
     settings_local, "ACTIVITYPUB_CELERY_BROKER_URL", ""
@@ -19,65 +20,139 @@ activitypub_app.conf.task_routes = {"pod.activitypub.tasks.*": {"queue": "activi
 activitypub_app.conf.task_always_eager = CELERY_TASK_ALWAYS_EAGER
 activitypub_app.conf.task_eager_propagates = CELERY_TASK_ALWAYS_EAGER
 
+logger = get_task_logger(__name__)
+
 
 @activitypub_app.task()
 def task_follow(following_id):
-    from .network import follow
+    from .models import Following
+    from .network import send_follow_request
 
-    return follow(following_id)
+    following = Following.objects.get(id=following_id)
+    return send_follow_request(following)
 
 
 @activitypub_app.task()
 def task_index_videos(following_id):
+    from .models import Following
     from .network import index_videos
 
-    return index_videos(following_id)
+    following = Following.objects.get(id=following_id)
+    return index_videos(following)
 
 
 @activitypub_app.task()
-def task_send_accept_request(follow_actor, follow_object, follow_id):
-    from .network import send_accept_request
+def task_handle_inbox_follow(username, data):
+    from .network import handle_inbox_follow
 
-    return send_accept_request(follow_actor, follow_object, follow_id)
-
-
-@activitypub_app.task()
-def task_read_announce(actor, object_id):
-    from .network import read_announce
-
-    return read_announce(actor, object_id)
+    return handle_inbox_follow(ap_follow=data)
 
 
 @activitypub_app.task()
-def task_external_video_update(video):
+def task_handle_inbox_accept(username, data):
+    from pod.activitypub.utils import ap_object
+
+    from .network import follow_request_accepted
+
+    obj = ap_object(data["object"])
+    if obj["type"] == "Follow":
+        return follow_request_accepted(ap_follow=data["object"])
+
+    logger.debug("Ignoring inbox 'Accept' action for '%s' object", obj["type"])
+
+
+@activitypub_app.task()
+def task_handle_inbox_reject(username, data):
+    from pod.activitypub.utils import ap_object
+
+    from .network import follow_request_rejected
+
+    obj = ap_object(data["object"])
+    if obj["type"] == "Follow":
+        return follow_request_rejected(ap_follow=data["object"])
+
+    logger.debug("Ignoring inbox 'Reject' action for '%s' object", obj["type"])
+
+
+@activitypub_app.task()
+def task_handle_inbox_announce(username, data):
+    from pod.activitypub.utils import ap_object
+
+    from .network import external_video_added_by_actor, external_video_added_by_channel
+
+    obj = ap_object(data["object"])
+    actor = ap_object(data["actor"])
+
+    if obj["type"] == "Video":
+        if actor["type"] in ("Application", "Person"):
+            return external_video_added_by_actor(ap_video=obj, ap_actor=actor)
+
+        elif actor["type"] in ("Group",):
+            return external_video_added_by_channel(ap_video=obj, ap_channel=actor)
+
+    logger.debug("Ignoring inbox 'Announce' action for '%s' object", obj["type"])
+
+
+@activitypub_app.task()
+def task_handle_inbox_update(username, data):
+    from pod.activitypub.utils import ap_object
+
     from .network import external_video_update
 
-    return external_video_update(video)
+    obj = ap_object(data["object"])
+    if obj["type"] == "Video":
+        return external_video_update(ap_video=obj)
+
+    logger.debug("Ignoring inbox 'Update' action for '%s' object", obj["type"])
 
 
 @activitypub_app.task()
-def task_external_video_deletion(object_id):
+def task_handle_inbox_delete(username, data):
+    from pod.activitypub.utils import ap_object
+
     from .network import external_video_deletion
 
-    return external_video_deletion(object_id)
+    obj = ap_object(data["object"])
+    if obj["type"] == "Video":
+        return external_video_deletion(ap_video=obj)
+
+    logger.debug("Ignoring inbox 'Delete' action for '%s' object", obj["type"])
 
 
 @activitypub_app.task()
 def task_broadcast_local_video_creation(video_id):
-    from .network import broadcast_local_video_creation
+    from pod.video.models import Video
 
-    return broadcast_local_video_creation(video_id)
+    from .models import Follower
+    from .network import send_video_announce_object
+
+    video = Video.objects.get(id=video_id)
+    # TODO: maybe delegate in subtasks for better performance?
+    for follower in Follower.objects.all():
+        send_video_announce_object(video, follower)
 
 
 @activitypub_app.task()
 def task_broadcast_local_video_update(video_id):
-    from .network import broadcast_local_video_update
+    from pod.video.models import Video
 
-    return broadcast_local_video_update(video_id)
+    from .models import Follower
+    from .network import send_video_update_object
+
+    video = Video.objects.get(id=video_id)
+    # TODO: maybe delegate in subtasks for better performance?
+    for follower in Follower.objects.all():
+        send_video_update_object(video, follower)
 
 
 @activitypub_app.task()
 def task_broadcast_local_video_deletion(video_id):
-    from .network import broadcast_local_video_deletion
+    from pod.video.models import Video
 
-    return broadcast_local_video_deletion(video_id)
+    from .models import Follower
+    from .network import send_video_delete_object
+
+    video = Video.objects.get(id=video_id)
+    # TODO: maybe delegate in subtasks for better performance?
+    for follower in Follower.objects.all():
+        send_video_delete_object(video, follower)
