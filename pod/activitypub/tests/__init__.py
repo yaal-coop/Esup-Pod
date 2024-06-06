190aa27b19b45001e82c8f8ccc7fd0c3e9a76beb
Éloi Rivard <eloi@yaal.coop>
2024-06-06 15:20:26 +0200

    better unit tests

--- pod/activitypub/tests/__init__.py
+++ pod/activitypub/tests/__init__.py
@@ -25,32 +25,44 @@ class ActivityPubTestCase(TestCase):
     def tearDown(self):
         del self.admin_user
 
-    @httmock.urlmatch(path=r"/.well-known/nodeinfo")
+    @httmock.urlmatch(path=r"^/.well-known/nodeinfo$")
     def mock_nodeinfo(self, url, request):
         with open("pod/activitypub/tests/fixtures/nodeinfo.json") as fd:
             payload = json.load(fd)
 
         return httmock.response(200, payload)
 
-    @httmock.urlmatch(path=r"/accounts/peertube/inbox")
-    def mock_inbox(self, url, request):
-        return httmock.response(204, "")
-
-    @httmock.urlmatch(path=r"/accounts/peertube")
+    @httmock.urlmatch(path=r"^/accounts/peertube$")
     def mock_application_actor(self, url, request):
         with open("pod/activitypub/tests/fixtures/application_actor.json") as fd:
             payload = json.load(fd)
 
         return httmock.response(200, payload)
 
-    @httmock.urlmatch(path=r"/videos/watch.*")
+    @httmock.urlmatch(path=r"^/accounts/peertube/inbox$")
+    def mock_inbox(self, url, request):
+        return httmock.response(204, "")
+
+    @httmock.urlmatch(path=r"^/accounts/peertube/outbox$")
+    def mock_outbox(self, url, request):
+        if url.query == "page=1":
+            fixture = "pod/activitypub/tests/fixtures/outbox-page-1.json"
+        else:
+            fixture = "pod/activitypub/tests/fixtures/outbox.json"
+
+        with open(fixture) as fd:
+            payload = json.load(fd)
+
+        return httmock.response(200, payload)
+
+    @httmock.urlmatch(path=r"^/videos/watch.*")
     def mock_get_video(self, url, request):
         with open("pod/activitypub/tests/fixtures/peertube_video.json") as fd:
             payload = json.load(fd)
 
         return httmock.response(200, payload)
 
-    @httmock.urlmatch(path=r"/video-channels/.*")
+    @httmock.urlmatch(path=r"^/video-channels/.*")
     def mock_get_channel(self, url, request):
         with open("pod/activitypub/tests/fixtures/channel.json") as fd:
             payload = json.load(fd)
