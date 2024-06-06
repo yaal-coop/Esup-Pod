6cfcfd6a6e94498236b6e32936fdb86a8c16c44f
Éloi Rivard <eloi@yaal.coop>
2024-06-06 15:20:26 +0200

    better unit tests

--- pod/activitypub/tests/test_admin.py
+++ pod/activitypub/tests/test_admin.py
@@ -1,12 +1,16 @@
 from . import ActivityPubTestCase
+from unittest.mock import patch
 import httmock
 from pod.activitypub.models import Following
 
 
 class AdminActivityPubTestCase(ActivityPubTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.force_login(self.admin_user)
+
     def test_send_federation_request(self):
         """Nominal case test for the admin 'send_federation_request' action."""
-        self.client.force_login(self.admin_user)
 
         following = Following.objects.create(
             object="http://peertube.test", status=Following.Status.NONE
@@ -25,7 +29,44 @@ class AdminActivityPubTestCase(ActivityPubTestCase):
                 },
                 follow=True,
             )
+
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            str(list(response.context["messages"])[0]),
+            "The federation requests have been sent",
+        )
 
         following.refresh_from_db()
         self.assertEqual(following.status, Following.Status.REQUESTED)
+
+    def test_reindex_videos(self):
+        """Nominal case test for the admin 'reindex_videos' action."""
+
+        following = Following.objects.create(
+            object="http://peertube.test", status=Following.Status.NONE
+        )
+
+        with httmock.HTTMock(
+            self.mock_nodeinfo,
+            self.mock_application_actor,
+            self.mock_outbox,
+            self.mock_get_video,
+        ), patch(
+            "pod.activitypub.network.ap_video_to_external_video"
+        ) as ap_video_to_external_video:
+            response = self.client.post(
+                "/admin/activitypub/following/",
+                {
+                    "action": "reindex_videos",
+                    "_selected_action": [
+                        str(following.id),
+                    ],
+                },
+                follow=True,
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                str(list(response.context["messages"])[0]),
+                "The video indexations have started",
+            )
+            self.assertTrue(ap_video_to_external_video.called)
