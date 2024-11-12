4bc97d2287a7a1a9278c0b99862561b37249a7e8
Loan Robert <loan.rbrt@gmail.com>
2024-11-12 16:22:13 +0100

    Fix typo

--- dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
+++ dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Launching commands into pod-dev"
-until nc -z pod.localhost 8000; do echo waiting for pod-back vizojfiezofjiezojfieozj; sleep 10; done;
+until nc -z pod.localhost 8000; do echo waiting for pod-back; sleep 10; done;
 # Worker ActivityPub
 env DJANGO_SETTINGS_MODULE=pod.settings \
     celery --app pod.activitypub.tasks worker --loglevel INFO --queues activitypub --concurrency 1 --hostname activitypub
