9aeedacede7a644b42d49944dfa76d86a0c9c65f
Loan Robert <loan.rbrt@gmail.com>
2024-11-12 17:33:41 +0100

    Fix django settings

--- dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
+++ dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
@@ -2,6 +2,5 @@
 echo "Launching commands into pod-dev"
 until nc -z pod.localhost 8000; do echo waiting for pod-back; sleep 10; done;
 # Worker ActivityPub
-env DJANGO_SETTINGS_MODULE=pod.settings \
-    celery --app pod.activitypub.tasks worker --loglevel INFO --queues activitypub --concurrency 1 --hostname activitypub
+celery --app pod.activitypub.tasks worker --loglevel INFO --queues activitypub --concurrency 1 --hostname activitypub
 sleep infinity
