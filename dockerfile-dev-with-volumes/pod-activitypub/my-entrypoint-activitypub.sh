758ceb8ec5c470a8d5ac26a4e131cfd7d8a6c124
Loan Robert <loan.rbrt@gmail.com>
2024-11-12 16:00:24 +0100

    Fix preprod feedback

--- dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
+++ dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "Launching commands into pod-dev"
-until nc -z pod-back 8000; do echo waiting for pod-back; sleep 10; done;
+until nc -z pod.localhost 8000; do echo waiting for pod-back vizojfiezofjiezojfieozj; sleep 10; done;
 # Worker ActivityPub
 env DJANGO_SETTINGS_MODULE=pod.settings \
-    python -m watchdog.watchmedo auto-restart --directory pod --pattern '*.py' --recursive --\
     celery --app pod.activitypub.tasks worker --loglevel INFO --queues activitypub --concurrency 1 --hostname activitypub
 sleep infinity
