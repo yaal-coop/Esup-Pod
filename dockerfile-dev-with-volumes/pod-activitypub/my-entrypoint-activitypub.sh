f5e6889c2d47261967c57e4bd07611a1a2f1c90a
Éloi Rivard <eloi@yaal.coop>
2025-09-11 14:19:28 +0200

    watchdog restarts the AP worker

--- dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
+++ dockerfile-dev-with-volumes/pod-activitypub/my-entrypoint-activitypub.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+watchmedo auto-restart --directory=/usr/src/app --pattern=*.py --recursive -- \
 celery --app pod.activitypub.tasks worker --loglevel INFO --queues activitypub --concurrency 1 --hostname activitypub
 sleep infinity
